59db4ee99ea9d13580275ee0197fc93bd5279057
Alex <alexander@baron-online.eu>
2020-05-22 20:53:19 +0200

    Vanilla Tailwind

---
+++ src/components/Test.tsx
@@ -0,0 +1,5 @@
+import tw from 'twin.macro';
+
+export const Button = tw.button`
+  bg-blue-500 hover:bg-blue-700 text-white p-2 rounded
+`;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
 import * as React from 'react';
 import { Page } from '../components/Page';
+import { Button } from '../components/Test';
 
 const IndexPage: React.FC = () => (
   <Page>
     <h1>Welcome! This is my page!</h1>
+    <Button>Hello Tailwind!</Button>
   </Page>
 );
 
