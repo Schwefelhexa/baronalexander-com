4a1c4ddf80cc9ba32b8aba147296eeca6fac7a16
Alex <alexander@baron-online.eu>
2020-05-25 17:23:02 +0200

    Nice 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,15 +1,20 @@
 import * as React from 'react';
-import { Link } from 'gatsby';
 import { Page } from '../components/Page';
+import { Hero, HeroText, HeroSubtext } from '../components/designsystem/Hero';
+import { Link } from '../components/designsystem/Link';
 
 const NotFoundPage: React.FC = () => (
   <Page name="Not found!">
-    <h1>404: Page not found.</h1>
-    <p>
-      You&apos;ve hit the void.
-      {' '}
-      <Link to="/">Go back.</Link>
-    </p>
+    <Hero>
+      <HeroText>
+        Page not found!
+      </HeroText>
+      <HeroSubtext>
+        This page does not exist.
+        {' '}
+        <Link to="/">Go back?</Link>
+      </HeroSubtext>
+    </Hero>
   </Page>
 );
 
