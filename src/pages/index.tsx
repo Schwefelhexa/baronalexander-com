10067b4257f53953a8e5f1c7ac0a6d547ebda800
Schwefelhexa <alexander@baron-online.eu>
2020-09-25 15:55:00 +0200

    Next.js setup

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,79 +1,15 @@
-import * as React from 'react';
-import { graphql } from 'gatsby';
-import { Page } from '../components/Page';
-import { ButtonGroup } from '../components/designsystem/Button';
-import { HeroText, Hero, HeroSubtext } from '../components/designsystem/Hero';
-import { Divtext } from '../components/designsystem/Divtext';
-import { Link, LinkButton } from '../components/designsystem/Link';
+import React from 'react'
+import Head from 'next/head'
 
-interface IIndexPageProps {
-  data: IndexPageQueryResponse;
-}
-const IndexPage: React.FC<IIndexPageProps> = ({ data }) => (
-  <Page className="homepage">
-    <Hero>
-      <HeroText>
-        {data.site.siteMetadata.author.name.split(' ').reduce<React.ReactNode[]>((all, word, i, arr) => (
-          [
-            ...all,
-            /* eslint-disable-next-line react/no-array-index-key */
-            <span key={i * 2}>{word}</span>,
-            /* eslint-disable-next-line react/no-array-index-key */
-            <br key={i + 1} />,
-          ]
-        ), []).slice(0, -1)}
-      </HeroText>
-      <HeroSubtext>
-        <Divtext>
-          <span>{data.site.siteMetadata.author.work.description}</span>
-          <span>@</span>
-          <span>
-            <Link to={data.site.siteMetadata.author.work.place.page} external>{data.site.siteMetadata.author.work.place.name}</Link>
-          </span>
-        </Divtext>
-      </HeroSubtext>
-    </Hero>
-
-    <ButtonGroup>
-      <LinkButton to="/projects" variant="secondary">Stuff I do</LinkButton>
-      <LinkButton to="/contact">Contact me</LinkButton>
-    </ButtonGroup>
-  </Page>
+const Home: React.FC = () => (
+  <div>
+    <Head>
+      <title>Create Next App</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+    <div className="w-screen h-screen flex justify-center items-center">
+      <h1 className="text-red-500 text-4xl">Hello World!</h1>
+    </div>
+  </div>
 );
-
-export default IndexPage;
-
-export const query = graphql`
-query IndexPageQuery {
-  site {
-    siteMetadata {
-      author {
-        name
-        work {
-          description
-          place {
-            name
-            page
-          }
-        }
-      }
-    }
-  }
-}
-`;
-interface IndexPageQueryResponse {
-  site: {
-    siteMetadata: {
-      author: {
-        name: string;
-        work: {
-          description: string;
-          place: {
-            name: string;
-            page: string;
-          }
-        }
-      }
-    }
-  }
-}
+export default Home;
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
+  },
+  purge: [
+    'src/**/*.tsx',
+    'src/**/*.ts',
+  ],
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
