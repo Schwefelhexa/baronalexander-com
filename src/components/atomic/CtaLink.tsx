ab33e14ede3fec8c7182b0aee603ada49daaefcb
Schwefelhexa <alexander@baron-online.eu>
2020-09-28 07:52:58 +0200

    Animate CTA Link

--- src/components/atomic/CtaLink.tsx
+++ src/components/atomic/CtaLink.tsx
@@ -6,13 +6,22 @@ interface Props {
   href: string;
   children: string;
   subtle?: boolean;
+  noAnimation?: boolean;
 }
-const CtaLink: React.FC<Props> = ({ href, children, subtle = false }) => (
+const CtaLink: React.FC<Props> = ({
+  href,
+  children,
+  subtle = false,
+  noAnimation = false,
+}) => (
   <Link href={href}>
     <span
       className={classNames('block w-full h-full text-center cursor-pointer', {
         'text-white bg-green-400 rounded-lg shadow-lg py-2 lg:py-3': !subtle,
         'font-bold text-blue-600': subtle,
+        'transition-colors duration-100 ease-in-out': !noAnimation,
+        'hover:bg-green-500': !subtle && !noAnimation,
+        'hover:text-blue-700': subtle && !noAnimation,
       })}
     >
       {children}
