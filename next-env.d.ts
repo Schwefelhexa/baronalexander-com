3a93652eabb1fdb603ba790aaaa7a0bd30cd0a91
Alexander Baron <alexander@baron-online.eu>
2020-12-11 15:57:42 +0100

    (feat) Project list

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,2 +1,9 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
+
+declare namespace NodeJS {
+  interface ProcessEnv {
+    readonly CONTENTFUL_SPACE: string;
+    readonly CONTENTFUL_TOKEN: string;
+  }
+}
--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -1,10 +1,48 @@
+import { gql } from '@apollo/client';
+import { GetServerSideProps } from 'next';
+import Link from 'next/link';
 import React from 'react';
 import Page from '../../components/layout/Page';
+import client from '../../core/graphql';
+import {
+  ProjectsListQuery,
+  ProjectsListQueryVariables,
+} from '../../generated/graphql';
 
-interface Props {}
-const ProjectPage: React.FC<Props> = () => (
+interface Props {
+  projects: ProjectsListQuery['projectCollection'];
+}
+const ProjectPage: React.FC<Props> = ({ projects }) => (
   <Page>
     <h1>Projects</h1>
+    <ul>
+      {projects?.items.map((project) => (
+        <li key={project!.slug!}>
+          <Link href={`/projects/${project!.slug!}`}>
+            <a>{project?.name}</a>
+          </Link>
+        </li>
+      ))}
+    </ul>
   </Page>
 );
 export default ProjectPage;
+
+const query = gql`
+  query ProjectsList {
+    projectCollection(limit: 500) {
+      items {
+        name
+        slug
+      }
+    }
+  }
+`;
+export const getServerSideProps: GetServerSideProps<Props> = async () => {
+  const { data } = await client.query<
+    ProjectsListQuery,
+    ProjectsListQueryVariables
+  >({ query });
+
+  return { props: { projects: data.projectCollection } };
+};
