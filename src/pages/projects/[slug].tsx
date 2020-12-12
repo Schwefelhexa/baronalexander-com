8c82b6810aa2948f9b918cf1663754d6ef15a9e4
Alexander Baron <alexander@baron-online.eu>
2020-12-12 12:53:36 +0100

    (feat) Basic project page

---
+++ src/pages/projects/[slug].tsx
@@ -0,0 +1,93 @@
+import { gql } from '@apollo/client';
+import { GetStaticPaths, GetStaticProps } from 'next';
+import { useRouter } from 'next/dist/client/router';
+import React from 'react';
+import Page from '../../components/layout/Page';
+import client from '../../core/graphql';
+import { Always } from '../../core/typeutil';
+import {
+  ProjectQuery,
+  ProjectQueryVariables,
+  SlugsQuery,
+  SlugsQueryVariables,
+} from '../../generated/graphql';
+
+type Project = Always<
+  Always<Always<ProjectQuery['projectCollection']>['items']>[0]
+>;
+
+interface Props {
+  project: Project;
+}
+const ProjectPage: React.FC<Props> = ({ project }) => {
+  const { isFallback } = useRouter();
+  if (isFallback) return null;
+  return (
+    <Page>
+      <h1>{project.name}</h1>
+    </Page>
+  );
+};
+export default ProjectPage;
+
+const PROJECT_QUERY = gql`
+  query Project($slug: String!) {
+    projectCollection(where: { slug: $slug }, limit: 1) {
+      items {
+        name
+        description {
+          json
+        }
+        demo
+        techStackCollection {
+          items {
+            name
+            icon {
+              url
+            }
+            website
+          }
+        }
+      }
+    }
+  }
+`;
+export const getStaticProps: GetStaticProps<Props> = async ({ params }) => {
+  const { data, errors } = await client.query<
+    ProjectQuery,
+    ProjectQueryVariables
+  >({
+    query: PROJECT_QUERY,
+    variables: {
+      slug: params!.slug as string,
+    },
+  });
+  if (errors) console.error(errors);
+  if (data.projectCollection?.items.length === 0) return { notFound: true };
+
+  return { props: { project: data.projectCollection?.items[0]! } };
+};
+
+const SLUGS_QUERY = gql`
+  query Slugs {
+    projectCollection {
+      items {
+        slug
+      }
+    }
+  }
+`;
+export const getStaticPaths: GetStaticPaths = async () => {
+  const { data } = await client.query<SlugsQuery, SlugsQueryVariables>({
+    query: SLUGS_QUERY,
+  });
+
+  return {
+    paths: data
+      .projectCollection!.items.map((slug) =>
+        slug ? '/projects/' + slug.slug : null
+      )
+      .filter((slug) => slug !== null) as string[],
+    fallback: false,
+  };
+};
