bef69841f96e8d1633386fdf8999f0f261794be3
Alexander Baron <alexander@baron-online.eu>
2020-12-13 18:11:51 +0100

    (fix) Bottom padding for scrolling pages

--- src/components/layout/Page.tsx
+++ src/components/layout/Page.tsx
@@ -3,12 +3,19 @@ import classNames from 'classnames';
 
 interface Props {
   noPadding?: boolean;
+  noScroll?: boolean;
   children: React.ReactNode;
 }
-const Page: React.FC<Props> = ({ children, noPadding = false }) => (
+const Page: React.FC<Props> = ({
+  children,
+  noPadding = false,
+  noScroll = false,
+}) => (
   <div
-    className={classNames('h-full font-medium leading-tight', {
+    className={classNames('font-medium leading-tight', {
       'px-8 py-7 lg:mx-auto lg:py-12 lg:w-2/3 max-w-5xl': !noPadding,
+      'h-full': noScroll,
+      'min-h-full': !noScroll,
     })}
   >
     {children}
