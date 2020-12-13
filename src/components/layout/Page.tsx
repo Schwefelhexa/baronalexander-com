c9a3c416db81808e3c6ca84dfb5424cd3e7a019b
Alexander Baron <alexander@baron-online.eu>
2020-12-13 19:59:20 +0100

    (feat) Logged in indicator

--- src/components/layout/Page.tsx
+++ src/components/layout/Page.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import classNames from 'classnames';
+import { useSession } from 'next-auth/client';
+import Link from 'next/link';
 
 interface Props {
   noPadding?: boolean;
@@ -10,15 +12,30 @@ const Page: React.FC<Props> = ({
   children,
   noPadding = false,
   noScroll = false,
-}) => (
-  <div
-    className={classNames('font-medium leading-tight', {
-      'px-8 py-7 lg:mx-auto lg:py-12 lg:w-2/3 max-w-5xl': !noPadding,
-      'h-full': noScroll,
-      'min-h-full': !noScroll,
-    })}
-  >
-    {children}
-  </div>
-);
+}) => {
+  const [session, loading] = useSession();
+
+  return (
+    <div className="relative w-full h-full">
+      <div
+        className={classNames('font-medium leading-tight relative', {
+          'px-8 py-7 lg:mx-auto lg:py-12 lg:w-2/3 max-w-5xl': !noPadding,
+          'h-full': noScroll,
+          'min-h-full': !noScroll,
+        })}
+      >
+        {children}
+      </div>
+      {!loading && session !== null && (
+        <div className="fixed right-0 bottom-0 mr-8 mb-3">
+          <Link href="/auth">
+            <a className="text-primary font-semibold md:text-xl xl:text-2xl">
+              Logged in
+            </a>
+          </Link>
+        </div>
+      )}
+    </div>
+  );
+};
 export default Page;
