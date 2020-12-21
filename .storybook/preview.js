d662e7e50a55dabe5bca7524e33dc55b07e3df7a
Alexander Baron <alexander@baron-online.eu>
2020-12-21 13:20:35 +0100

    (feat) Storybook: Dark mode toggle

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,30 @@
+import React from 'react';
 import '../src/styles/tailwind.css';
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
 };
+
+export const globalTypes = {
+  theme: {
+    name: 'Theme',
+    defaultValue: 'light',
+    toolbar: {
+      icon: 'lightning',
+      // array of plain string values or MenuItem shape (see below)
+      items: ['light', 'dark'],
+    },
+  },
+};
+
+const withThemeProvider = (Story, context) => {
+  const dark = context.globals.theme === 'dark';
+  return (
+    <div className={dark ? 'dark' : ''}>
+      <div className="dark:bg-dark">
+        <Story {...context} />
+      </div>
+    </div>
+  );
+};
+export const decorators = [withThemeProvider];
